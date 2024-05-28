9c68d9a93ed63d03378a128597b5ee90ebef0a1b
klopovaevgeniya <144274553+klopovaevgeniya@users.noreply.github.com>
2024-05-28 14:59:40 +0300

    Add files via upload

---
+++ prac8/SerBibliotek/Ser.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+using System.Xml.Serialization;
+
+namespace SerBibliotek
+{
+    public class Ser
+    {
+        public static void Serialize<T>(T data, string filePath)
+        {
+            using (var stream = new FileStream(filePath, FileMode.Create))
+            {
+                var serializer = new XmlSerializer(typeof(T));
+                serializer.Serialize(stream, data);
+            }
+        }
+
+        public static T Deserialize<T>(string filePath)
+        {
+            using (var stream = new FileStream(filePath, FileMode.Open))
+            {
+                var serializer = new XmlSerializer(typeof(T));
+                return (T)serializer.Deserialize(stream);
+            }
+        }
+    }
+}
---
+++ prac8/prac8/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using SerBibliotek;
+
+namespace prac8
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // Получаем текст из TextBlock
+            string text = textBlock.Text;
+
+            // Сериализуем текст
+            Ser.Serialize(text, "textdata.xml");
+            MessageBox.Show("Текст успешно сериализован!");
+
+            // Десериализуем текст
+            if (System.IO.File.Exists("textdata.xml"))
+            {
+                string deserializedText = Ser.Deserialize<string>("textdata.xml");
+                MessageBox.Show($"Текст успешно десериализован:\n{deserializedText}");
+            }
+            else
+            {
+                MessageBox.Show("Файл данных для десериализации не найден!");
+            }
+        }
+    }
+}
